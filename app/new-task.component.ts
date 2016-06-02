40d966ea42400bc46fd87db2ce79513f69cdb7cb
Amanda Daly and Connor Cummings <techdaly@gmail.com>
2016-06-02 14:30:43 -0700

    connected pipeline, minor bug on create and add task functionality

---
+++ app/new-task.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {Task} from './task.model';
+
+@Component({
+  selector: 'new-task',
+  outputs: ['onSubmitNewTask'],
+  template: `
+  <div class="task-form">
+    <h3>Create Task:</h3>
+    <input placeholder="Description" class="col-sm-8 input-lg" #newDescription>
+    <button (click)="addTask(newDescription)" class="btn-success btn-lg add-button">Add</button>
+    <new-task (onSubmitNewTask)="createTask($event)"></new-task>
+  </div>
+  `
+})
+export class NewTaskComponent {
+  taskList;
+  public onSubmitNewTask: EventEmitter<String>;
+  constructor(){
+    this.onSubmitNewTask = new EventEmitter();
+  }
+  addTask(userDescription: HTMLInputElement){
+    this.onSubmitNewTask.emit(userDescription.value);
+    userDescription.value = "";
+  }
+  createTask(description: string): void {
+  this.taskList.push(
+    new Task(description, this.taskList.length)
+  );
+}
+}
